2554db4d67f90293bab5e835baa263b87b5976c0
KeemROHDev <reekar121a@gmail.com>
2021-01-06 13:15:56 +0000

    fixed client not being executed in commands

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ client.on("message", message => {
   setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
   try {
-    command.execute(message, args);
+    command.execute(message, args, client);
   } catch (error) {
     console.error(error);
     message.reply("there was an error trying to execute that command!");
